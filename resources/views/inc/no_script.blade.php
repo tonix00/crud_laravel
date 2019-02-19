b6299d4c38cf4c89f2bab5c1f5babd619133f94b
Antonio Comique <tonmyx@yahoo.com>
2019-02-19 12:51:47 +0800

    update in progress

--- resources/views/inc/no_script.blade.php
+++
@@ -1,4 +0,0 @@
-<div id="fb-root"></div>
-<noscript class="for-no-js">JavaScript is not supported</noscript>
-<div class="for-old">You are using old browser.</div>
-<input type="hidden" value="./" id="js-base-url">
